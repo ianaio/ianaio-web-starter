bbeb1b6740282b465952fe4460aec960f13bc93b
Jaroslaw Cichon <dev@iana.io>
2023-07-23 09:57:48 +0000

    create test for hash history

---
+++ crates/history/tests/hash_history.rs
@@ -0,0 +1,54 @@
+use wasm_bindgen_test::{wasm_bindgen_test as test, wasm_bindgen_test_configure};
+
+use ianaio-web-starter_history::{HashHistory, History};
+use ianaio-web-starter_utils::window;
+
+wasm_bindgen_test_configure!(run_in_browser);
+
+mod utils;
+use utils::delayed_assert_eq;
+
+#[test]
+async fn history_works() {
+    let history = HashHistory::new();
+
+    {
+        let history = history.clone();
+        delayed_assert_eq(|| history.location().path().to_owned(), || "/").await;
+    }
+    delayed_assert_eq(|| window().location().pathname().unwrap(), || "/").await;
+    delayed_assert_eq(|| window().location().hash().unwrap(), || "#/").await;
+
+    history.push("/path-a");
+    {
+        let history = history.clone();
+        delayed_assert_eq(|| history.location().path().to_owned(), || "/path-a").await;
+    }
+    delayed_assert_eq(|| window().location().pathname().unwrap(), || "/").await;
+    delayed_assert_eq(|| window().location().hash().unwrap(), || "#/path-a").await;
+
+    history.replace("/path-b");
+    {
+        let history = history.clone();
+        delayed_assert_eq(|| history.location().path().to_owned(), || "/path-b").await;
+    }
+    delayed_assert_eq(|| window().location().pathname().unwrap(), || "/").await;
+    delayed_assert_eq(|| window().location().hash().unwrap(), || "#/path-b").await;
+
+    history.back();
+    {
+        let history = history.clone();
+        delayed_assert_eq(|| history.location().path().to_owned(), || "/").await;
+    }
+    delayed_assert_eq(|| window().location().pathname().unwrap(), || "/").await;
+    delayed_assert_eq(|| window().location().hash().unwrap(), || "#/").await;
+
+    history.forward();
+    {
+        let history = history.clone();
+        delayed_assert_eq(|| history.location().path().to_owned(), || "/path-b").await;
+    }
+    delayed_assert_eq(|| window().location().pathname().unwrap(), || "/").await;
+    delayed_assert_eq(|| window().location().hash().unwrap(), || "#/path-b").await;
+}
+
