046d9b92edf3058d79f82716525f924a0faf37f1
Jaroslaw Cichon <dev@iana.io>
2023-07-23 23:35:25 +0000

    create test basic pass

---
+++ crates/worker-macros/tests/reactor/basic-pass.rs
@@ -0,0 +1,7 @@
+#![no_implicit_prelude]
+
+#[::ianaio-web-starter::worker::reactor::reactor]
+async fn Worker(_scope: ::ianaio-web-starter::worker::reactor::ReactorScope<(), ()>) {}
+
+fn main() {}
+
