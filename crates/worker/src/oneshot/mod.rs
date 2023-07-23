6b4f58d3c60107ef335a976f38b3fe8b45744338
Jaroslaw Cichon <dev@iana.io>
2023-07-23 22:06:58 +0000

    create module

---
+++ crates/worker/src/oneshot/mod.rs
@@ -0,0 +1,38 @@
+//! A future-based worker that for each input, one output is produced.
+//!
+//! ## Example
+//!
+//! ```rust, no_run
+//! use ianaio-web-starter_worker::oneshot::oneshot;
+//! use ianaio-web-starter_worker::Spawnable;
+//!
+//! #[oneshot]
+//! async fn Squared(input: u32) -> u32 {
+//!     input.pow(2)
+//! }
+//!
+//! # async {
+//! // consuming the worker
+//! let mut squared_bridge = Squared::spawner().spawn("...");
+//! assert_eq!(squared_bridge.run(2).await, 4);
+//! # };
+//! ```
+
+mod bridge;
+mod registrar;
+mod spawner;
+mod traits;
+mod worker;
+
+pub use bridge::OneshotBridge;
+pub use registrar::OneshotRegistrar;
+pub use spawner::OneshotSpawner;
+pub use traits::Oneshot;
+
+/// Creates an oneshot worker.
+///
+/// See [module level documentation](self) for more information.
+#[doc(inline)]
+#[cfg(feature = "futures")]
+pub use ianaio-web-starter_worker_macros::oneshot;
+
